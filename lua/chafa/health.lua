3c8f412cf6fdfcb002f64813a75907f22b57ac36
Frederick Zhang <frederick888@tsundere.moe>
2025-05-16 10:11:56 +1000

    fix: migrate to new vim.health APIs

--- lua/chafa/health.lua
+++ lua/chafa/health.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 M.check = function()
-  vim.health.report_start("chafa.nvim health check")
+  vim.health.start("chafa.nvim health check")
   if vim.fn.executable("chafa") == 1 then
-    vim.health.report_ok("no issues found")
+    vim.health.ok("no issues found")
   else
-    vim.health.report_error("chafa executable not found")
-    vim.health.report_info("Follow installations instructions at https://hpjansson.org/chafa/download/")
+    vim.health.error("chafa executable not found")
+    vim.health.info("Follow installations instructions at https://hpjansson.org/chafa/download/")
   end
 end
 
